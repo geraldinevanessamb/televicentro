6ea80ced9d4cb94d6eef40bf81e05f33e95e216f
Dulce Abigail Mejía <39814572+dulcemejiav@users.noreply.github.com>
2018-09-09 21:59:20 -0600

    Add files via upload

---
+++ js/my-app.js
@@ -0,0 +1,35 @@
+// Initialize your app
+var myApp = new Framework7({
+	root:'#app',
+	theme: 'md', //ios, md, auto
+    modalTitle: 'Framework7', 	//title for modals
+	panel: {
+    	swipe: 'left',
+  	},
+	cache:false,
+	pushState:false,
+	swipeBackPage:true,
+	material: true,
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/productos/',
+    	url: 'productos.html',
+    	name: 'productos',
+  		}
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+
+
+
+ 
+
